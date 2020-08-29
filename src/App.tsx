499f5bc390be8e3dcf79def285685c63cdfab3ba
Luke 10x <spam@luke10x.dev>
2020-08-29 12:47:55 +0300

    🔍 Handle folder URI path with ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <SettingsProvider>
-        <Router>
+        <Router basename="/toptrumps">
           <Switch>
             <Route path="/game">
               <BattleProvider>
